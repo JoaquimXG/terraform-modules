fe60a0d1615d3b9e3b621df26a60620ee7cf7351
Joaquim <49433786+JoaquimXG@users.noreply.github.com>
2022-02-06 21:53:27 +0000

    Adding egress rule for security group in instance module

--- ansible-instance/security-group.tf
+++ ansible-instance/security-group.tf
@@ -22,6 +22,14 @@ resource aws_security_group sg {
         ipv6_cidr_blocks = ["::/0"]
     }
 
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
     tags = {
         Name = "sg-ssh_http_${var.tag_name}"
     }
